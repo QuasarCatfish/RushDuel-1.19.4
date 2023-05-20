57fbf2c9bb811fc3432e2d5ad86eee5a2641ea9b
Quas <ctsn99@gmail.com>
2023-05-20 18:39:26 -0400

    Initial commit

---
+++ src/main/java/net/quas/rushduel/RushDuelMod.java
@@ -0,0 +1,51 @@
+package net.quas.rushduel;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(RushDuelMod.MOD_ID)
+public class RushDuelMod {
+
+	public static final String MOD_ID = "rushduel";
+	private static final Logger LOGGER = LogUtils.getLogger();
+
+	public RushDuelMod() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::commonSetup);
+
+		MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+	}
+
+	private void commonSetup(final FMLCommonSetupEvent event) {
+	}
+
+	private void addCreative(CreativeModeTabEvent.BuildContents event) {
+	}
+
+	// You can use SubscribeEvent and let the Event Bus discover methods to call
+	@SubscribeEvent
+	public void onServerStarting(ServerStartingEvent event) {
+
+	}
+
+	// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+	@Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class ClientModEvents {
+		@SubscribeEvent
+		public static void onClientSetup(FMLClientSetupEvent event) {
+
+		}
+	}
+}
